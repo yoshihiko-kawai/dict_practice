0596e0a8649184ee3703612f2b5c71128a49c9d9
yoshihiko-kawai <kawai@alumni.tus.ac.jp>
2019-09-19 15:10:20 +0900

    辞書型の練習

---
+++ fruit.py
@@ -0,0 +1,22 @@
+def main():
+    fruits_dict = {
+        "リンゴ": "Apple",
+        "ブドウ": "Grape",
+        "みかん": "Orange",
+    }
+
+    # Grapeを出力するコードを描く
+
+    print(fruits_dict["ブドウ"])
+
+    # Orangeを出力するコードを描く
+
+    print(fruits_dict["みかん"])
+
+    # Appleを出力するコードを描く
+
+    print(fruits_dict["リンゴ"])
+
+
+if __name__ == '__main__':
+    main()
